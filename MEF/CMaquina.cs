ae17e80300a8868cd1f196fc7d89a628f9e235b8
Black995 <54086005+Black995@users.noreply.github.com>
2020-06-25 22:05:37 -0400

    Búsqueda del objeto más cercano

--- MEF/CMaquina.cs
+++ MEF/CMaquina.cs
@@ -41,9 +41,6 @@ namespace MEF
 		// Variable del indice del objeto que buscamos
 		private int indice;
 
-		// Variable para la energia;
-		private int energia;
-
 		// Creamos las propiedades necesarias
 		public int CoordX 
 		{
@@ -70,7 +67,6 @@ namespace MEF
 			x=320;		// Coordenada X
 			y=240;		// Coordenada Y
 			indice=-1;	// Empezamos como si no hubiera objeto a buscar
-			energia=800;
 		}
 
 		public void Inicializa(ref S_objeto [] Pobjetos, S_objeto Pbateria)
@@ -103,8 +99,6 @@ namespace MEF
 						Estado=(int)estados.NBUSQUEDA;
 
 					}
-					else if(energia<400) // Checamos condicion de transicion
-						Estado=(int)estados.IRBATERIA;
 
 					break;
 
@@ -128,14 +122,9 @@ namespace MEF
 					if(x==bateria.x && y==bateria.y)				
 						Estado=(int)estados.RECARGAR;
 
-					if(energia==0)
-						Estado=(int)estados.MUERTO;
-
 					break;
 
 				case (int)estados.RECARGAR:
-					// Llevamos a cabo la accion del estado
-					Recargar();
 
 					// Hacemos la transicion
 					Estado=(int)estados.BUSQUEDA;
@@ -143,20 +132,12 @@ namespace MEF
 					break;
 
 				case (int)estados.MUERTO:
-					// Llevamos a cabo la accion del estado
-					Muerto();
-
-					// No hay condicion de transicion
 					
 					break;
 
 				case (int)estados.ALEATORIO:
 					// Llevamos a cabo la accion del estado
 					Aleatorio();
-
-					// Verificamos por transicion
-					if(energia==0)
-						Estado=(int)estados.MUERTO;
 
 					break;
 
@@ -179,9 +160,6 @@ namespace MEF
 			else if(y>objetos[indice].y)
 				y--;
 
-			// Disminuimos la energia
-			energia--;
-
 		}
 
 		public void NuevaBusqueda()
@@ -190,13 +168,23 @@ namespace MEF
 			// Verificamos que haya otro objeto a buscar
 			indice=-1;
 
-			// Recorremos el arreglo buscando algun objeto activo
-			for(int n=0;n<10;n++)
-			{
-				if(objetos[n].activo==true)
-					indice=n;
-			}
-		}
+            double cercania, mas_cercano=99999;
+            // Buscamos el objeto más cerca
+            for (int n = 0; n < 10; n++)
+            {
+
+                if (objetos[n].activo == true)
+                {
+                    //Calculamos cuál pelota está más cercana al Jugador
+                    cercania = Math.Sqrt(Math.Pow((x - objetos[n].x), 2) + Math.Pow((y - objetos[n].y), 2));
+                    if (mas_cercano > cercania)
+                    {
+                        mas_cercano = cercania;
+                        indice = n;
+                    }
+                }
+            }
+        }
 
 		public void Aleatorio()
 		{
@@ -212,8 +200,6 @@ namespace MEF
 			// Modificamos la posicion al azar
 			x+=nx-1;
 			y+=ny-1;
-
-			energia--;
 
 		}
 
@@ -232,27 +218,7 @@ namespace MEF
 			else if(y>bateria.y)
 				y--;
 
-			// Disminuimos la energia
-			energia--;
-
 		}
-
-		public void Recargar()
-		{
-			// En esta funcion colocamos la logica del estado Recargar
-			energia=1000;
-
-		}
-
-		public void Muerto()
-		{
-			// En esta funcion colocamos la logica del estado Muerto
-
-			// Sonamos un beep de la computadora
-
-
-		}
-
 
 
 	}
