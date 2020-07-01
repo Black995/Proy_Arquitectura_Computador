24aeaa9c80c3f5d5b4aea7e8f078ca0f7c50e80d
David <dalfonzo18@gmail.com>
2020-07-01 16:30:20 -0400

    asdasd

--- MEF/Form1.cs
+++ MEF/Form1.cs
@@ -18,25 +18,25 @@ namespace MEF
         private MenuItem mnuParo;
         private Timer timer1;
         private IContainer components;
+        
 
         // Creamos un objeto para la maquina de estados finitos
         private CMaquina maquina = new CMaquina();
         private CMaquina maquina2 = new CMaquina();
-
+        
 
         // Objetos necesarios
         public S_objeto[] ListaObjetos = new S_objeto[10];
         public S_objeto[] ListaObjetos2 = new S_objeto[10];
-
         public S_objeto MiBateria;
-
+        
         public Form1()
         {
             //
             // Necesario para admitir el Diseñador de Windows Forms
             //
             InitializeComponent();
-
+            
             //
             // TODO: agregar código de constructor después de llamar a InitializeComponent
             //
@@ -70,7 +70,7 @@ namespace MEF
 
         }
 
-
+     
         protected override void Dispose(bool disposing)
         {
             if (disposing)
@@ -142,18 +142,21 @@ namespace MEF
             // 
             // timer1
             // 
-            this.timer1.Interval = 5;
+            this.timer1.Interval = 1;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
             // 
             this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImage = global::MEF.Properties.Resources.map;
             this.ClientSize = new System.Drawing.Size(692, 500);
+            this.DoubleBuffered = true;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Menu = this.mainMenu1;
             this.Name = "Form1";
-            //this.Text = "Maquina de estados finitos";
             this.Text = "Marico el que lo lea";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
             this.ResumeLayout(false);
 
@@ -205,34 +208,42 @@ namespace MEF
             Font fuente = new Font("Arial", 16);
             Font fuente2 = new Font("Arial", 24);
 
-            SolidBrush brocha = new SolidBrush(Color.Black);
+            SolidBrush brocha = new SolidBrush(Color.White);
             SolidBrush brochaVerde = new SolidBrush(Color.Green);
             SolidBrush brochaRoja = new SolidBrush(Color.Red);
 
+            Rectangle rect;
+            Image coin = Image.FromFile("coin.png");
+            Image yellowGhost = Image.FromFile("mario.gif");
+            Image redGhost = Image.FromFile("dragon.gif");
+            Image fresa = Image.FromFile("estrella.gif");
 
 
-            // Dibujamos el robot
-            if (maquina.EstadoM == (int)CMaquina.estados.MUERTO)
-                e.Graphics.DrawRectangle(Pens.Black, maquina.CoordX - 4, maquina.CoordY - 4, 20, 20);
-            else
-                e.Graphics.DrawRectangle(Pens.Green, maquina.CoordX - 4, maquina.CoordY - 4, 20, 20);
-
-            if (maquina2.EstadoM == (int)CMaquina.estados.MUERTO)
-                e.Graphics.DrawRectangle(Pens.Black, maquina2.CoordX - 10, maquina2.CoordY - 10, 20, 20);
-            else
-                e.Graphics.DrawRectangle(Pens.Red, maquina2.CoordX -10, maquina2.CoordY - 10, 20, 20);
-
+                //Fatasma amarillo
+                 rect = new Rectangle(maquina.CoordX - 4, maquina.CoordY - 4, 40, 40);
+                 e.Graphics.DrawImage(yellowGhost, rect);
+                //Fantasma rojo
+                rect = new Rectangle(maquina2.CoordX - 4, maquina2.CoordY - 4, 40, 40);
+                e.Graphics.DrawImage(redGhost, rect);
 
             // Dibujamos los objetos
             for (int n = 0; n < 10; n++)
                 if (ListaObjetos[n].activo == true)
-                    e.Graphics.DrawRectangle(Pens.Indigo, ListaObjetos[n].x - 4, ListaObjetos[n].y - 4, 20, 20);
+                {
+                    rect = new Rectangle(ListaObjetos[n].x - 4, ListaObjetos[n].y - 4, 20, 20);
+                    e.Graphics.DrawImage(coin,rect);
+                }
+
             for (int n = 0; n < 10; n++)
                 if (ListaObjetos2[n].activo == true)
-                    e.Graphics.DrawRectangle(Pens.Blue, ListaObjetos2[n].x - 10, ListaObjetos2[n].y - 10, 20, 20);
+                {
+                    rect = new Rectangle(ListaObjetos2[n].x - 4, ListaObjetos2[n].y - 4, 20, 20);
+                    e.Graphics.DrawImage(coin, rect);
+                }
 
             // Dibujamos la bateria
-            e.Graphics.DrawRectangle(Pens.IndianRed, MiBateria.x - 4, MiBateria.y - 4, 20, 20);
+            rect = new Rectangle(MiBateria.x - 4, MiBateria.y - 4, 30, 30);
+            e.Graphics.DrawImage(fresa, rect);
 
             // Indicamos el estado en que se encuentra la maquina
             e.Graphics.DrawString("Verde -> " + maquina.EstadoM.ToString(), fuente, brocha, 10, 10);
@@ -241,13 +252,23 @@ namespace MEF
             if(maquina.EstadoM == 4)
             {
                 timer1.Enabled = false;
-                e.Graphics.DrawString("GANÓ VERDE" , fuente2, brochaVerde, 250, 200);
+                e.Graphics.DrawString("GANÓ MARIO" , fuente2, brochaRoja, 250, 200);
             }
             if (maquina2.EstadoM == 4)
             {
                 timer1.Enabled = false;
-                e.Graphics.DrawString("GANÓ ROJO" , fuente2, brochaRoja, 250, 200);
+                e.Graphics.DrawString("GANÓ YOSHI" , fuente2, brochaVerde, 250, 200);
             }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
 
         }
     }
